d0d1dabb3091817d012d8f9d9bf47e97ecd6bceb
EslebanPerez <eslebanperez@gmail.com>
2023-08-05 21:04:47 -0600

    Create cat con dto

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+
+  constructor(
+    @InjectRepository(Cat)
+    private readonly catRepository:Repository<Cat>
+  ){}
+  
+
+  async create(createCatDto: CreateCatDto) {
+    try {
+      return await this.catRepository.save(createCatDto);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all cats`;
+  async findAll() {
+    return this.catRepository.find();
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} cat`;
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
+  async update(id: number, updateCatDto: UpdateCatDto) {
     return `This action updates a #${id} cat`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} cat`;
   }
 }
