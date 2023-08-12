66dabdcda2f5b564c7a1a0030929df4e72f02704
EslebanPerez <eslebanperez@gmail.com>
2023-08-12 01:27:04 -0600

    Generando una nueva entidad y agregando en el module

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  //@Column({primary: true, generated: true})
+  id: number;
+  
+  @Column({unique: true})
+  name: string;
+
+  @Column({unique: true, nullable: false})
+  email: string;
+
+  @Column({nullable: false})
+  password: string;
+
+  @Column({default: 'user'})
+  rol: string
+
+  @DeleteDateColumn()
+  deleteAt: Date;
+}
