a50f2eb882d083e5e8810efa4a38a0ba23d2627f
EslebanPerez <eslebanperez@gmail.com>
2023-08-12 03:42:14 -0600

    Inyección de dependencias en authService para usar métodos del userService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly userService: UsersService 
+  ){}
+
   register() {
     return 'register';
   }
